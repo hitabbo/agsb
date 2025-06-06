56b29f3be5e7f966f23767bfeda2568e5bf05816
hitabbo <hitabo@163.com>
2025-06-07 01:49:35 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,12 +31,12 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = ""                     # UUID，留空则自动生成
+USER_NAME = "xowaiox"         # 用户名
+UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
+DOMAIN = "streamlit.959818.xyz"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiNmYwZmIyNDM1ZDE5ZTU0OTJlZWFmMGJhOTMxYWRlNDQiLCJ0IjoiMzAzODFhMDItOTA4Mi00ZjA3LTkwNDYtZTNkMmMzZGI5MzNhIiwicyI6Ik1XRTBZelF6TmpndE9HUTFZeTAwWkdVM0xXRXpNV010TkRNNU1XWmhZbVZrTXpRNCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
-# ======================================
+# =========================================
 
 # 添加命令行参数解析
 def parse_args():
